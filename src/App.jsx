c68fb328713e20ea3633beedd8237cfd16d48054
tomasgrinstein <163456994+tomasgrinstein@users.noreply.github.com>
2025-09-25 20:37:40 -0300

    animaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+
 import Header from './components/Header';
 import Apa from './components/apa';
 import Hacer from './components/hacer';
@@ -6,21 +8,92 @@ import Reader from './components/reader';
 import ComoSeUsa from './components/ComoSeUsa';
 import Metas from './components/Metas';
 import Ayudanos from './components/ayudanos';
-import Promo from './components/promo';   // <-- Importamos Promo
-import Footer from './components/Footer'; // <-- Importamos Footer
+import Promo from './components/promo';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
-      <Header />
-      <Apa />
-      <Hacer />
-      <Reader />
-      <ComoSeUsa/>
-      <Metas />
-      <Ayudanos/>
-      <Promo />    {/* <-- Agregamos Promo */}
-      <Footer />   {/* <-- Agregamos Footer y reemplazamos a ParteAbajo */}
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+        viewport={{ amount: 0.2 }}
+      >
+        <Header />
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+        viewport={{ amount: 0.2 }}
+      >
+        <Apa />
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, x: -50 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.8 }}
+        viewport={{ amount: 0.2 }}
+      >
+        <Hacer />
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, scale: 0.9 }}
+        whileInView={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.8 }}
+        viewport={{ amount: 0.2 }}
+      >
+        <Reader />
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+        viewport={{ amount: 0.2 }}
+      >
+        <ComoSeUsa />
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+        viewport={{ amount: 0.2 }}
+      >
+        <Metas />
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, x: 50 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.8 }}
+        viewport={{ amount: 0.2 }}
+      >
+        <Ayudanos />
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, scale: 0.8 }}
+        whileInView={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.9 }}
+        viewport={{ amount: 0.2 }}
+      >
+        <Promo />
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+        viewport={{ amount: 0.2 }}
+      >
+        <Footer />
+      </motion.div>
     </>
   );
 }
